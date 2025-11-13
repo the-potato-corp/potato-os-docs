bb66940f28d7b8fce74cc9b2935a34b1877ee271
redisnotbluedev <ededmaude@gmail.com>
2025-11-13 12:20:26 +1030

    change make thingy & rebuild

--- build/dirhtml/_sources/developer_guide/potato_os.rst.txt
+++
@@ -1,8 +0,0 @@
-Native APIs
-===========
-
-These are modules exposed to STARCH specifically on PotatoOS. Support on other OSes might not exist:
-
-.. note::
-
-    Nothing here yet...
--- make.bat
+++ make.bat
@@ -8,7 +8,8 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 set SOURCEDIR=source
-set BUILDDIR=build
+REM Change the default build folder name to docs
+set BUILDDIR=docs
 
 %SPHINXBUILD% >NUL 2>NUL
 if errorlevel 9009 (
@@ -25,10 +26,21 @@ if errorlevel 9009 (
 
 if "%1" == "" goto help
 
-%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+REM --- MODIFICATION START ---
+REM We bypass the -M option for all cases and use direct -b command
+
+REM Check if the command is "help" (which uses -M help)
+if "%1" == "help" goto help
+
+REM Otherwise, assume the first argument is the builder name (e.g., html, dirhtml, singlehtml)
+REM and run the specific build command directly.
+%SPHINXBUILD% -b %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
 
+REM --- MODIFICATION END ---
+
 :help
+REM Help still uses -M as it handles all help output types elegantly
 %SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 
 :end
